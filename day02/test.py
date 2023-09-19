8fc2a9765b7791856ec1ad5b8f2ff0fc8cd24bd3
raduga9199 <raduga9199@gmail.com>
2023-09-19 12:56:39 -0400

    commit

--- day02/test.py
+++ day02/test.py
@@ -1,6 +1,23 @@
 # import functions
-from utility import sum
+from utility import sum, calculate
 
-result = sum(1,2)
 
-print(result)
+print(sum(1,2))
+print(calculate(2,3,'*'))
+
+
+import utility
+
+print(utility.sum(10, 20))
+print(utility.concat("A", 1, 2))
+print(utility.calculate(20, 30, '*'))
+
+import utility as u
+
+print(u.sum(10, 20))
+print(u.concat("A", 1, 2))
+print(u.calculate(20, 30, '*'))
+
+from  utility import sum as s
+
+print(s(20, 30, 40))
