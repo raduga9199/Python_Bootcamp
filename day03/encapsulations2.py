f3d366d0859341cafc3e6988fd203624b785c5f8
raduga9199 <raduga9199@gmail.com>
2023-09-20 12:55:40 -0400

    encapsulation

--- day03/encapsulations2.py
+++ day03/encapsulations2.py
@@ -1,18 +1,21 @@
 class Person:
 
-    def __init__(self, name: str = 'Unknown', age: int = 1):
+    def __init__(self, name: str = 'uknown', age: int = 0):
         self.__name = None
         self.__age = None
-        self.set_name(name)
-        self.set_age(age)
+        self.person_name = name
+        self.person_age = age
 
-    def get_name(self) -> str:
+    @property
+    def person_name(self):
         if self.__name is None or self.__name == '' or type(self.__name) != str:
             raise RuntimeError(f'Invalid name has been set: {self.__name}')
 
         return self.__name
 
-    def set_name(self, name: str):
+    @person_name.setter
+    def person_name(self, name):
+
         if type(name) != str:
             raise RuntimeError(f'Person name must be string')
 
@@ -21,40 +24,23 @@ class Person:
 
         self.__name = name
 
-    def get_age(self) -> int:
+    @property
+    def person_age(self):
         return self.__age
 
-    def set_age(self, age):
-
+    @person_age.setter
+    def person_age(self, age):
         if age < 0 or age > 150:
-            raise RuntimeError(f'Inavlid age {age}')
+            raise f'Invalid {age}'
         self.__age = age
+
 
     def __str__(self):
         return f'{type(self).__name__}{ str(self.__dict__).replace("_Person__", "")}'
 
 
 
-
-
 person1 = Person()
+person1.person_name = 'Daniel'
 
-print(person1.get_name())
-print(person1.get_age())
-
-person2 = Person('James')
-
-print(person2.get_name())
-print(person2.get_age())
-
-
-person3 = Person('Hazel', 27)
-
-print(person3.get_name())
-print(person3.get_age())
-
-print('------------------')
-
-print(person1)
-print(person2)
-print(person3)
+print(person1.person_name)
